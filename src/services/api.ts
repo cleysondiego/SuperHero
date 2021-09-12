e959996f640b5016057a3d178eaff2f87e9933e0
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-09-12 15:12:29 -0300

    Melhorando a forma de pegar os parametros necessarios para chamadas a api da Marvel.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,15 @@ import axios from 'axios';
 import md5 from 'md5';
 import { REACT_APP_MARVEL_API_PRIVATE_KEY, REACT_APP_MARVEL_API_PUBLIC_KEY } from '@env';
 
-const timestamp = Date.now();
-const hash = md5(`${timestamp}${REACT_APP_MARVEL_API_PRIVATE_KEY}${REACT_APP_MARVEL_API_PUBLIC_KEY}`);
+const getParams = () => {
+  const ts = Date.now();
+  const hash = md5(ts + REACT_APP_MARVEL_API_PRIVATE_KEY + REACT_APP_MARVEL_API_PUBLIC_KEY);
+  const apikey = REACT_APP_MARVEL_API_PUBLIC_KEY;
+
+  return { ts, hash, apikey };
+}
 
 export const api = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public',
-  params: {
-    apikey: `${REACT_APP_MARVEL_API_PUBLIC_KEY}`,
-    ts: timestamp,
-    hash: hash,
-    limit: 20
-  }
-})
+  params: getParams(),
+});
